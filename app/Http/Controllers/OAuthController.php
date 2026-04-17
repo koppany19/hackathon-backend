3dcd4190e7fc85e02482ade9a2e6637fd26d2e9a
Koppany <jozsa.koppany.tibor@student.ms.sapientia.ro>
2026-04-17 14:05:09 +0300

    log added

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
 class OAuthController extends Controller
@@ -35,7 +36,8 @@ class OAuthController extends Controller
             ]);
 
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Google auth failed'], 401);
+            Log::error('Google OAuth failed', ['error' => $e->getMessage()]);
+            return response()->json(['message' => $e->getMessage()], 401);
         }
     }
 }
